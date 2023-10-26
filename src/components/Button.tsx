7511c52db9a17770577d95cbd4719d9a95e2c2b9
Leandro Bianchini <leandrobianchini@alunos.utfpr.edu.br>
2023-10-25 23:00:28 -0300

    Fix: responsivity

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,5 @@
 export default function Button(props: React.PropsWithChildren) {
-   return <button className="border border-black rounded-full px-8 py-3">
+   return <button className="border border-black rounded-full px-8 py-3 tracking-wider">
     {props.children || "Learn More"}
    </button>    
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,10 @@ import Tile from '../components/Tile'
 import { Source_Sans_3 } from 'next/font/google'
 import Card from '../components/Card'
 import Social from '../components/Social'
-const sans = Source_Sans_3({ subsets: ['latin'], weight: ["300", '700'] })
+const sans = Source_Sans_3({ subsets: ['latin'], weight: ["300", "400", '700'] })
 
 export default function Home() {
-  return <div className={` bg-white text-black min-h-screen px-24 ${sans.className} font-light max-w-[1440px]`}>
+  return <div className={` bg-white text-black min-h-screen md:px-24 ${sans.className} font-normal md:font-light max-w-[1440px]`}>
     <main>
       <section className='mb-24'>
         <Card img='/image/IMAGE1.png' invert buttonText="GET STARTED" >
@@ -17,9 +17,9 @@ export default function Home() {
         <Card header='Pharetra etiam nulla' img='/image/IMAGE4.png'/>
       </section>
       <section className='text-center'>
-        <h2 className='mb-4'>Ipsum sed consequat</h2>
+        <h2 className='mb-4 text-4xl'>Ipsum sed consequat</h2>
         <p className='mb-10'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id ante sed ex pharetra lacinia sit amet vel massa. Donec facilisis laoreet nulla eu bibendum. Donec ut ex risus. Fusce lorem lectus, pharetra pretium massa et, hendrerit vestibulum odio lorem ipsum.</p>
-        <div className='grid grid-cols-3 divide-x-2 divide-y-2 border-r-2 border-b-2 mx-10'>
+        <div className='grid md:grid-cols-2 xl:grid-cols-3 divide-x-2 divide-y-2 border-r-2 border-b-2 mx-4 md:mx-10'>
           <Tile icon={1} title='Lorem'></Tile>
           <Tile icon={2} title='Ipsum'></Tile>
           <Tile icon={3} title='Dolor'></Tile>
@@ -35,7 +35,7 @@ export default function Home() {
         </div>
       </section>
     </main>
-    <footer className='flex flex-col mx-auto justify-center items-center py-32'>
+    <footer className='flex flex-col mx-auto justify-center items-center py-16 pb-10 md:py-32'>
       <div className='flex space-x-4 py-6'>
         <Social icon={1}></Social>
         <Social icon={2}></Social>
@@ -43,7 +43,7 @@ export default function Home() {
         <Social icon={4}></Social>
         <Social icon={5}></Social>
       </div>
-      <p className='font-bold text-sm'>© Untitled. Design: ALUNOS WEB.</p>
+      <p className='font-bold text-sm'>© Untitled. Design: <u>ALUNOS WEB.</u></p>
     </footer>
   </div>
 }
